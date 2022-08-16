599659ee2093c4337eb5cc8b554c4bbc6556b802
github2109 <quanghiep2109@gmail.com>
2022-08-16 16:17:16 +0700

    first

---
+++ backend/app.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const mongoose = require("mongoose");
+const config = require("./utils/config");
+const logger = require("./utils/logger");
+const middleware = require("./utils/middleware");
+
+logger.info("connecting to", config.MONGODB_URL);
+mongoose
+  .connect(config.MONGODB_URL)
+  .then(() => {
+    logger.info("connected to MongoDB");
+  })
+  .catch((error) => {
+    logger.error("error connecting to MongoDB:", error.message);
+  });
+
+app.use(cors());
+app.use(express.static("build"));
+app.use(express.json());
+app.use(middleware.requestLogger);
+app.use(middleware.tokenExtractor);
+//routes
+readdirSync("./routes").map((r) => app.use("api/", require("./routes/" + r)));
+
+module.exports = app;
