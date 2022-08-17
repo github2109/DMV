aa69ac168f97fe90d32001f68f5603c4cb84ec00
github2109 <quanghiep2109@gmail.com>
2022-08-17 09:09:56 +0700

    model

---
+++ backend/controllers/module.js
@@ -0,0 +1,49 @@
+const mongoose = require("mongoose");
+
+const moduleSchema = mongoose.Schema({
+  state: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "State",
+  },
+  license: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "License",
+  },
+  question: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Question",
+    },
+  ],
+  name: {
+    type: String,
+    required: [true, "Module name is required"],
+    trim: true,
+    text: true,
+  },
+  titleDescription: {
+    type: String,
+    required: [true, "Title description is required"],
+    trim: true,
+  },
+  contentDescription: {
+    type: String,
+    required: [true, "Content description is required"],
+    trim: true,
+  },
+  imageDescription: {
+    type: String,
+    required: [true, "Image description is required"],
+    trim: true,
+  },
+  position: {
+    type: Number,
+    required: [true, "Position is required"],
+  },
+  isPremium: {
+    type: Boolean,
+    required: [true, "Premium is required"],
+  },
+});
+
+module.exports = mongoose.model("Module", moduleSchema);
---
+++ backend/controllers/question.js
@@ -0,0 +1,49 @@
+const mongoose = require("mongoose");
+
+const questionSchema = mongoose.Schema({
+  module: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Module",
+  },
+  questionContent: {
+    type: String,
+    required: [true, "Question content is required"],
+    trim: true,
+    text: true,
+  },
+  answers: [
+    {
+      key: Number,
+      content: String,
+      isCorrect: Boolean,
+    },
+    {
+      key: Number,
+      content: String,
+      isCorrect: Boolean,
+    },
+    {
+      key: Number,
+      content: String,
+      isCorrect: Boolean,
+    },
+    {
+      key: Number,
+      content: String,
+      isCorrect: Boolean,
+    },
+  ],
+  isTestQuestion: {
+    type: Boolean,
+    required: [true, "Is test question is required"],
+  },
+  image: {
+    type: String,
+    required: [true, "Image is required"],
+  },
+  handBook: {
+    type: String,
+  },
+});
+
+module.exports = mongoose.model("Question", questionSchema);
