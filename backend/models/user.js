c2498e39e47985eff036d1605ecbf06fc17f9e93
github2109 <quanghiep2109@gmail.com>
2022-08-17 09:47:51 +0700

    9:47 17/8

---
+++ backend/models/user.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+const userSchema = mongoose.Schema({
+  username: {
+    type: String,
+    required: [true, "Username is required"],
+  },
+  password: {
+    type: String,
+    required: [true, "Password is required"],
+  },
+  Role: {
+    type: String,
+    required: [true, "Role is required"],
+  },
+});
+
+module.exports = mongoose.model("User", userSchema);
