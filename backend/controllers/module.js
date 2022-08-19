9ae8751c9c18a4ab37cb5aa24e03189ae1b8b02c
github2109 <quanghiep2109@gmail.com>
2022-08-19 15:24:50 +0700

    3:24 19/8

--- backend/controllers/module.js
+++ backend/controllers/module.js
@@ -5,13 +5,15 @@ exports.getModuleByModuleId = async (moduleId) => {
   return await Module.findById(moduleId);
 };
 
-exports.updateModuleAfterRemoveQuestion = async (moduleId,questionId) => {
+exports.updateModuleAfterRemoveQuestion = async (moduleId, questionId) => {
   const module = await Module.findById(moduleId);
-  module.questions = module.questions.filter(question => question !== questionId);
+  module.questions = module.questions.filter(
+    (question) => question !== questionId
+  );
   return await module.save();
-}
+};
 
-exports.updateModuleAfterCreateQuestion = async (moduleId,questionId) => {
+exports.updateModuleAfterCreateQuestion = async (moduleId, questionId) => {
   const module = await Module.findById(moduleId);
   module.questions.push(questionId);
   return await module.save();
@@ -64,6 +66,16 @@ exports.getModuleByStateIdAndLicenseIdAPI = async (req, res, next) => {
   }
 };
 
+exports.getModuleByLicenseIdAPI = async (req, res, next) => {
+  try {
+    const { licenseId } = req.query;
+    const modules = await Module.find({ license:licenseId });
+    res.status(200).json(modules);
+  } catch (error) {
+    next(error);
+  }
+};
+
 exports.createModuleAPI = async (req, res, next) => {
   try {
     const module = req.body;
@@ -89,7 +101,7 @@ exports.deleteModuleAPI = async (req, res, next) => {
 
 exports.addModuleToStateAPI = async (req, res, next) => {
   try {
-    const { stateId,moduleId } = req.query;
+    const { stateId, moduleId } = req.query;
     const module = await Module.findById(moduleId);
     if (!module) {
       return res.status(404).json({ message: "Can't not find module" });
@@ -110,20 +122,22 @@ exports.addModuleToStateAPI = async (req, res, next) => {
   }
 };
 
-exports.removeModuleOfStateAPI = async (req, res,next) => {
+exports.removeModuleOfStateAPI = async (req, res, next) => {
   try {
-    const { stateId,moduleId } = req.query;
-    const module = await Module.findOne({_id: moduleId,states:stateId});
+    const { stateId, moduleId } = req.query;
+    const module = await Module.findOne({ _id: moduleId, states: stateId });
     if (!module) {
       return res.status(404).json({ message: "Can't find module" });
     }
-    module.states = module.states.filter(state => state !== stateId);
+    module.states = module.states.filter((state) => state !== stateId);
     await module.save();
-    return res.status(200).json({message: "Module removed from state successfully"});
+    return res
+      .status(200)
+      .json({ message: "Module removed from state successfully" });
   } catch (error) {
     next(error);
   }
-}
+};
 
 exports.getDescriptionByModuleIdAPI = async (req, res, next) => {
   try {
@@ -134,6 +148,16 @@ exports.getDescriptionByModuleIdAPI = async (req, res, next) => {
       imageDescription: 1,
     });
     res.status(200).json(description);
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.updateModuleAPI = async (req, res, next) => {
+  try {
+    const { moduleId } = req.params;
+    const moduleUpdated = await Module.findByIdAndUpdate(moduleId, req.body);
+    res.status(200).json(moduleUpdated);
   } catch (error) {
     next(error);
   }
--- backend/routes/module.js
+++ backend/routes/module.js
@@ -5,15 +5,19 @@ const {
   addModuleToStateAPI,
   getDescriptionByModuleIdAPI,
   deleteModuleAPI,
-  removeModuleOfStateAPI
+  removeModuleOfStateAPI,
+  updateModuleAPI,
+  getModuleByLicenseIdAPI
 } = require("../controllers/module");
 
 
 moduleRouter.get("/description/:moduleId", getDescriptionByModuleIdAPI);
+moduleRouter.get("/:licenseId?", getModuleByLicenseIdAPI);
 moduleRouter.get("/:stateId?/:licenseId?", getModuleByStateIdAndLicenseIdAPI);
 moduleRouter.post("/", createModuleAPI);
 moduleRouter.post("/add-module-to-state/:stateId?/:moduleId?", addModuleToStateAPI);
 moduleRouter.delete("/:moduleId", deleteModuleAPI);
 moduleRouter.delete("/remove-module-of-state/:stateId?/:moduleId?", removeModuleOfStateAPI);
+moduleRouter.put("/:moduleId", updateModuleAPI);
 
 module.exports = moduleRouter;
