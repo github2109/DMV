2c33e2776878ac4d3fbaa546c173541148b510a4
github2109 <quanghiep2109@gmail.com>
2022-08-24 19:57:29 +0700

    7:57 24/8

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { readdirSync } = require("fs");
+const path = require("path");
 const cors = require("cors");
 const mongoose = require("mongoose");
 const config = require("./utils/config");
@@ -26,7 +26,7 @@ mongoose
   });
 
 app.use(cors());
-app.use(express.static("build"));
+app.use(express.static(path.join(__dirname, "./build")));
 app.use(express.json());
 app.use(middleware.requestLogger);
 app.use(middleware.tokenExtractor);
